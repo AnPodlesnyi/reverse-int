7c5627ef5b590636428c0b88407cc40f67fd236c
Andrii Podlesnyi <an.podlesnyi@gmail.com>
2022-06-22 14:35:37 +0300

    init: start reverse-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numbplus = Math.abs(n)
+    let str = numbplus.toString().split('').reverse().join('');
+    let numb = Number(str)
+    return numb;
 }
